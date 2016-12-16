3a6be3a29a4a825810e6c469d88d590a65f82957
Jeff Dyer <jeffdyer@acm.org>
2016-12-16 14:11:17 -0700

    Support x \degree y ''

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {execSync} from "child_process";
 
-let id = "486153";  // Current best rule set
+let id = "489320";  // Current best rule set
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
