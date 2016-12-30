43bdfcefd8434d6131b2c799e44ecd6cd17d77b6
Jeff Dyer <jeffdyer@acm.org>
2016-12-30 12:57:57 -0800

    Fix equals

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {execSync} from "child_process";
 
-let id = "491153";  // Current best rule set
+let id = "491282";  // Current best rule set
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
