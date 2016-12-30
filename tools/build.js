f5ca3cef79a5ca324fb412a4b7008dd4e069e711
Jeff Dyer <jeffdyer@acm.org>
2016-12-30 10:51:45 -0800

    Fix typos

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {execSync} from "child_process";
 
-let id = "490549";  // Current best rule set
+let id = "491106";  // Current best rule set
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
