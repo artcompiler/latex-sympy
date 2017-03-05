95c9ac26695e52dc28d2784df24fbb3b1353f060
Jeff Dyer <jeffdyer@acm.org>
2017-03-05 16:44:23 +0100

    Make build compatible with new object format

--- src/core.js
+++ src/core.js
@@ -1390,7 +1390,7 @@ export let Core = (function () {
       valueNode = value != undefined ? Model.create(value, "spec") : undefined;
       Model.popEnv();
     } catch (e) {
-//      console.log(e.stack);
+      console.log(e.stack);
       pendingError = e;
     }
     let evaluate = function evaluate(solution, resume) {
@@ -1416,7 +1416,7 @@ export let Core = (function () {
         Model.popEnv();
         resume(null, result);
       } catch (e) {
-//        console.log(e.stack);
+        console.log(e.stack);
         let message = e.message;
         resume({
           result: null,
